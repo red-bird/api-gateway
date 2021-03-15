a7345139462ca5f13a05624e5339e13b37ee4c92
redbird <redbird8941@gmail.com>
2021-03-15 23:41:28 +0300

    Don't use reactor project, so dead end

--- src/main/java/com/redbird/apigateway/ApiGatewayApplication.java
+++ src/main/java/com/redbird/apigateway/ApiGatewayApplication.java
@@ -2,8 +2,12 @@ package com.redbird.apigateway;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @SpringBootApplication
+@EnableEurekaClient
+@EnableSwagger2
 public class ApiGatewayApplication {
 
 	public static void main(String[] args) {
