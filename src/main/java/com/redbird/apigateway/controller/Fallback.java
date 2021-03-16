36325d22003dccc79d36ac85a0356a89070293c0
redbird <redbird8941@gmail.com>
2021-03-16 05:20:38 +0300

    here we go again, add redis-rate-limiting, circuit-breaker & metrics

---
+++ src/main/java/com/redbird/apigateway/controller/Fallback.java
@@ -0,0 +1,21 @@
+package com.redbird.apigateway.controller;
+
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import reactor.core.publisher.Mono;
+
+@RestController
+@RequestMapping("/fallbacks")
+public class Fallback {
+
+    @PostMapping("/shopFallback")
+    public Mono<String> shopFallback() {
+        return Mono.just("shops-service unhealthy... Please try again later");
+    }
+
+    @PostMapping("/purchaseFallback")
+    public Mono<String> purchaseFallback() {
+        return Mono.just("shops-service unhealthy... Please try again later");
+    }
+}
